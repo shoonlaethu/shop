b36e5407fde5a802bcec8ccbfd03d70a71944c9e
shoon <thushoonlae2021@gmail.com>
2023-01-01 00:01:37 +0630

    oki

---
+++ js/master.js
@@ -0,0 +1,13 @@
+function abc(th){
+    // console.log("hello")
+
+    //let getsizes = document.getElementById("size").value
+    //console.log(getsizes);
+    console.log(th.value);
+
+    let size = th.value;
+    let getshoe = document.getElementById("shoe");
+    getshoe.style.width = size;
+
+}
+
